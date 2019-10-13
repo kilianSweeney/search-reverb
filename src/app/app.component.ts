1766a5d5189270815471d7231f5840f6b414dcb7
Kilian Sweeney <kilian_sweeney@yahoo.com>
2019-10-12 21:19:44 -0500

    no message

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,4 +1,5 @@
-import { Component } from '@angular/core';
+import { Component, OnChanges } from '@angular/core';
+import { ReverbService } from './services/reverb.service';
 
 @Component({
   selector: 'app-root',
@@ -6,5 +7,49 @@ import { Component } from '@angular/core';
   styleUrls: ['./app.component.css']
 })
 export class AppComponent {
+  constructor(private reverb: ReverbService) { }
   title = 'search-reverb';
+  data = [];
+  filteredData = [];
+  filters = {
+    body: '',
+    neck: ''
+  };
+  noData = true;
+  reverbUrl = 'assets/reverb.json';
+
+
+  getData = ($event) => {
+    if ($event === null || $event.target.value.length > 3){
+      this.noData = false;
+      this.reverb.http.get(this.reverbUrl).subscribe( (data) => {
+        this.data = data.data;
+        this.filteredData = this.updateFilteredData();
+      });
+    }
+  }
+
+  updateFilteredData = () => {
+    if (this.data.length === 0) { return []; }
+    if (this.filters.body === '' && this.filters.neck === '') { return this.data; }
+    const filteredData = [];
+    this.data.map( (item) => {
+      if (this.filters.neck.length > 0 && item.neck === this.filters.neck) {
+        if (this.filters.body === '' || this.filters.body.length > 0 && item.body === this.filters.body ) {filteredData.push(item); }
+      } else if (this.filters.body.length > 0 && item.body === this.filters.body ) {
+        if (this.filters.neck === '' || this.filters.neck.length > 0 && item.neck === this.filters.neck ) {filteredData.push(item); }
+      }
+    });
+    return filteredData;
+  }
+
+  setBodyFilter = (value) => {
+    this.filters.body = value;
+    this.filteredData = this.updateFilteredData();
+  }
+
+  setNeckFilter = (value) => {
+    this.filters.neck = value;
+    this.filteredData = this.updateFilteredData();
+  }
 }
--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -2,15 +2,24 @@ import { BrowserModule } from '@angular/platform-browser';
 import { NgModule } from '@angular/core';
 
 import { AppComponent } from './app.component';
+import { ClarityModule } from '@clr/angular';
+import { BrowserAnimationsModule } from '@angular/platform-browser/animations';
+import { HttpClientModule, HttpClient } from '@angular/common/http';
+import { ReverbService } from './services/reverb.service';
 
 @NgModule({
   declarations: [
     AppComponent
   ],
   imports: [
-    BrowserModule
+    BrowserModule,
+    HttpClientModule,
+    ClarityModule,
+    BrowserAnimationsModule
   ],
-  providers: [],
+  providers: [ReverbService],
   bootstrap: [AppComponent]
 })
-export class AppModule { }
+export class AppModule {
+
+}
